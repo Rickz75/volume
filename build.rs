a2fd0d0597749bc1ee542736358444299a1cc95a
Rickz75 <rickz75dev@gmail.com>
2022-03-18 14:23:58 +0200

    only build alsa.c on linux

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/native/alsa.c");
-    println!("cargo:rerun-if-changed=build.rs");
-    if let Err(e) = pkg_config::Config::new().probe("alsa") {
-        panic!("{}", e);
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rerun-if-changed=src/native/alsa.c");
+        println!("cargo:rerun-if-changed=build.rs");
+        if let Err(e) = pkg_config::Config::new().probe("alsa") {
+            panic!("{}", e);
+        }
+        cc::Build::new().file("src/native/alsa.c").compile("alsa");
     }
-    cc::Build::new().file("src/native/alsa.c").compile("alsa");
 }
